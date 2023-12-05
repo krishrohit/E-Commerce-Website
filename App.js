59f013448daad7c334fa93ed9b854f8b3dba4737
Rohit Fulari <137477659+krishrohit@users.noreply.github.com>
2023-12-05 17:14:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import PageNotFound from "./Pages/PageNotFound";
+import Menu from "./Pages/Menu";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact/>} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
